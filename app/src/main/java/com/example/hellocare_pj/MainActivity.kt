a62e8f807518b8d55a898c1ad0f55b11b31758c2
raeul0304 <subong0304@gmail.com>
2023-12-07 22:49:07 +0900

    12-05 sumin update

--- app/src/main/java/com/example/hellocare_pj/MainActivity.kt
+++ app/src/main/java/com/example/hellocare_pj/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.hellocare_pj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sigindoc)
+        setContentView(R.layout.activity_login)
     }
+    val currentUser = Firebase.auth.currentUser
+
 }
