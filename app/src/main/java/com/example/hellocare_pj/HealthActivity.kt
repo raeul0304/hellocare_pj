cda48f2027247f4fd9a9b306635f074af98b9693
raeul0304 <subong0304@gmail.com>
2023-12-23 11:00:53 +0900

    12-22 수민

--- app/src/main/java/com/example/hellocare_pj/HealthActivity.kt
+++ app/src/main/java/com/example/hellocare_pj/HealthActivity.kt
@@ -1,21 +1,26 @@
 package com.example.hellocare_pj
 
+import android.graphics.Color
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.example.hellocare_pj.databinding.ActivityHealthboardBinding
 import com.example.hellocare_pj.model.HealthBoardData
 import com.example.hellocare_pj.model.HealthData
 
-class HealthActivity: AppCompatActivity(){
+private const val TAG = "HealthFrag"
+class HealthActivity: AppCompatActivity() {
     private lateinit var binding: ActivityHealthboardBinding
     private lateinit var adapter: HealthAdapter
 
+
     val mDatas = mutableListOf<HealthBoardData>()
+    val datas = mutableListOf<HealthData>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +30,47 @@ class HealthActivity: AppCompatActivity(){
         initHealthRecyclerView()
     }
     fun initHealthRecyclerView(){
-        val adapter = HealthAdapter() //어댑터 객체 생성
+        val adapter: HealthAdapter = HealthAdapter() //어댑터 객체 생성
         adapter.datalist=mDatas // 데이터 넣기
         binding.healthRecyclerview.adapter=adapter //리사이클러뷰에 어댑터 연결
         binding.healthRecyclerview.layoutManager = LinearLayoutManager(this)
+        binding.healthRecyclerview.addItemDecoration(MyDecoration(30,50))
+
+   //     adapter!!.itemClick = object: HealthAdapter.ItemClick{
+   //         override fun onClick(view: View, position: Int) {
+   //             val clickedItem = datas[position]
+   //             val intent=Intent(baseContext, DetailActivity::class.java)
+   //             intent.putExtra("h1", "lkjh")
+   //             startActivity(intent)
+    //        }
+   //     }
     }
 
-    //리사이클러뷰 꾸미기
-
-
 }
+
+
+//리사이클러뷰 꾸미기
+class MyDecoration(private val horizontalSpacing : Int,
+    private val verticalSpacing: Int): RecyclerView.ItemDecoration(){
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        val index = parent.getChildAdapterPosition(view)
+        outRect.right = horizontalSpacing
+        outRect.left = horizontalSpacing
+        if(parent.getChildAdapterPosition(view) == 0){
+            outRect.top = verticalSpacing
+        }
+        outRect.bottom = verticalSpacing
+        if(index %2 ==0){
+            view.setBackgroundColor(Color.parseColor("#9FC6F9"))
+        }else{
+            view.setBackgroundColor(Color.parseColor("#EFEEEE"))
+        }
+        ViewCompat.setElevation(view, 20.0f)
+    }
+    }
--- app/src/main/java/com/example/hellocare_pj/HealthAdapter.kt
+++ app/src/main/java/com/example/hellocare_pj/HealthAdapter.kt
@@ -1,26 +1,30 @@
 package com.example.hellocare_pj
 
 import android.view.LayoutInflater
+import android.view.View.OnClickListener
 import android.view.ViewGroup
-import android.widget.ListAdapter
-import android.widget.SearchView.OnQueryTextListener
 import androidx.recyclerview.widget.RecyclerView
-import com.example.hellocare_pj.databinding.ActivityHealthboardBinding
 import com.example.hellocare_pj.databinding.HealthitemRecyclerviewBinding
 import com.example.hellocare_pj.model.HealthBoardData
-import com.example.hellocare_pj.model.HealthData
 
 //항목 뷰를 가지는 역할
-class MyViewHolder(val binding: HealthitemRecyclerviewBinding)
-    :RecyclerView.ViewHolder(binding.root)
+class MyViewHolder(val binding: HealthitemRecyclerviewBinding) :RecyclerView.ViewHolder(binding.root)
 
 //항목 구성자 어댑터
-class HealthAdapter ()
-    : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+class HealthAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    private var onClickListener: OnClickListener?=null
     //리사이클러뷰에서 사용할 데이터 미리 정의
     var datalist = mutableListOf<HealthBoardData>()
     val datas = mutableListOf<String>("원인 모를 발목 통증? '부주상골 증후군' 통증, 수술, 치료법",
-        "요즘 빈대 물리면 증상이 어떻게 되나요?")
+        "요즘 빈대 물리면 증상이 어떻게 되나요?",
+        "모공각화증, 각질이 쌓이는 원인을 없애야..",
+        "혼자만의 세계에 갇힌 아이, ‘자폐증’ 증상과 치료법",
+        "생리 중 성관계하면 임신 안된다? NO",
+        "긁히면 피부 부풀어 오르는 ‘피부 묘기증’,\n" + "냉찜질로 가라앉혀야",
+        "‘눈꺼풀 여드름’ 안검염, 만성 안구 건조증으로?",
+        "뒤로 젖힐 때 허리 아프다면? ‘척추분리증’ 의심해야",
+        "흔한 신생아 황달, 부모가 알아야 할 것은?")
 
     //항목 개수를 판단하기 위해 자동 호출
     override fun getItemCount(): Int = datas.size
@@ -29,7 +33,16 @@ class HealthAdapter ()
     private val imageResources = arrayOf(
         R.drawable.healthinfo_1,
         R.drawable.healthinfo_2,
+        R.drawable.healthinfo_3,
+        R.drawable.healthinfo_4,
+        R.drawable.healthinfo_5,
+        R.drawable.healthinfo_6,
+        R.drawable.healthinfo_7,
+        R.drawable.healthinfo_8
         )
+
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
         MyViewHolder(
             HealthitemRecyclerviewBinding.inflate(
@@ -37,14 +50,30 @@ class HealthAdapter ()
             )
         )
 
+
     //각 항목을 구성하기 위해 호출
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int){
         //뷰에 데이터 출력
+       // val item = items[position]
         val binding = (holder as MyViewHolder).binding
         val imageResource = imageResources[position]
         binding.itemImg.setImageResource(imageResource)
-        binding.itemTxt.text= datas[position]
+        binding.itemTxt.text = datas[position]
+
+
+        //holder.itemView.setOnClickListener{
+          //  if(onClickListener != null){
+            //    onClickListener!!.onClick(position,item)
+            //}
+       // }
     }
 
+ //   fun setOnClickListener(onClickListener: OnClickListener){
+ //       this.onClickListener=onClickListener
+ //   }
+ //   interface onClickListener {
+ //       fun onClick(position: Int, model: HealthData)
+ //   }
 }
+
 
